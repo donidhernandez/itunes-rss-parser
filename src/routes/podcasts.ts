5ba250d94eb34816fb7eac2f7b9e31e35507b85b
Adonai Dominguez <adonaidominguezes@gmail.com>
2023-08-28 15:42:19 +0200

    test

--- src/routes/podcasts.ts
+++ src/routes/podcasts.ts
@@ -7,8 +7,9 @@ router.get('/rss-parser', async (req, res) => {
   const { url } = req.query;
   res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-  const response = await podcastRssParser(url as string);
-  res.json(response);
+  res.send('Hi');
+  // const response = await podcastRssParser(url as string);
+  // res.json(response);
 });
 
 export default router;
