2cd9f295de412e1989c2984ae6b80dff35a88070
Adonai Dominguez <adonaidominguezes@gmail.com>
2023-08-28 16:04:13 +0200

    more tests

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,16 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = ['http://localhost:3000'];
 const options: cors.CorsOptions = {
-  origin: allowedOrigins,
+  origin: '*',
+  methods: 'GET',
 };
 
 app.use(cors(options));
 
 const PORT = 4000;
 
-app.get('/api', async (req, res) => {
+app.get('/', cors(), async (req, res) => {
   const { url } = req.query;
   const parser = new Parser();
   const feed = await parser.parseURL(url as string);
