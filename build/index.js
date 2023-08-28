49d53eb83ef35088ebf685585e5c2b2c2910a203
Adonai Dominguez <adonaidominguezes@gmail.com>
2023-08-28 16:54:59 +0200

    build has changed

--- build/index.js
+++ build/index.js
@@ -1,15 +1,42 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const rss_parser_1 = __importDefault(require("rss-parser"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+};
+app.use((0, cors_1.default)(corsOptions));
 const PORT = 4000;
-app.get('/api', (_req, res) => {
-    res.send('Successfully connected to the API server');
-});
+app.get('/api', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { url } = req.query;
+    const parser = new rss_parser_1.default();
+    const feed = yield parser.parseURL(url);
+    res.json(feed);
+}));
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
