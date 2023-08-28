5c300cc33871568afda30ba08c4042807c900ef4
Adonai Dominguez <adonaidominguezes@gmail.com>
2023-08-28 14:44:42 +0200

    cors activated

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/podcasts';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const PORT = 4000;
 
